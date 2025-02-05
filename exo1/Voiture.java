28c08614404f9f22253daba166092d75daf4dd5d
floriane-g <floriane.guillou@edu.ece.fr>
2025-02-05 15:28:10 +0100

    Ajout de exo1

---
+++ exo1/Voiture.java
@@ -0,0 +1,21 @@
+package exo1;
+
+public class Voiture {
+String marque;
+String modele;
+String couleur;
+
+// Méthodes
+void demarrer ()  {
+System.out.println("La voiture démarre");
+}
+
+void accelerer () {
+System.out.println("La voiture accelère");
+}
+
+void freiner () {
+System.out.println("La voiture freine");
+
+}
+}
