d14abc0bb9646e2ca2db9dda9483e6ee60082ed3
floriane-g <floriane.guillou@edu.ece.fr>
2025-02-11 15:24:13 +0100

    message

---
+++ gestion/GestionEtudiant.java
@@ -0,0 +1,12 @@
+
+package gestion;
+
+import java.util.ArrayList;
+
+public class GestionEtudiant {
+    private ArrayList<Etudiant> etudiants = new ArrayList<Etudiant>();
+
+    public ArrayList<Etudiant> listE () {
+        return this.etudiants;
+    }
+}
